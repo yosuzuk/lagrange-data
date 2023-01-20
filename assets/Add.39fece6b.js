9ed2cb071a7e7db63bdcec4e458a31cf1454e56a
yosuzuk <yosuzuk@users.noreply.github.com>
2023-01-20 20:04:13 +0100

    Updates

--- assets/Add.39fece6b.js
+++
@@ -1,1 +0,0 @@
-import{N as a,O as r,P as t}from"./index.5e0e4e47.js";var e={},v=r.exports;Object.defineProperty(e,"__esModule",{value:!0});var d=e.default=void 0,u=v(a),o=t,f=(0,u.default)((0,o.jsx)("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"}),"Add");d=e.default=f;export{d};
