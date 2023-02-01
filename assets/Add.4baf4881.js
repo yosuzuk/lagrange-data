d44ead715c30aa808f4056e254ea09474489c388
yosuzuk <yosuzuk@users.noreply.github.com>
2023-02-01 22:27:06 +0100

    Updates

--- assets/Add.4baf4881.js
+++
@@ -1,1 +0,0 @@
-import{P as a,Q as r,R as t}from"./index.5769b2e3.js";var e={},v=r.exports;Object.defineProperty(e,"__esModule",{value:!0});var d=e.default=void 0,u=v(a),o=t,f=(0,u.default)((0,o.jsx)("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"}),"Add");d=e.default=f;export{d};
