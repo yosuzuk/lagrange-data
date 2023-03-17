beb274f74e92c4cb6265fcd8e9c9dd9afde10536
yosuzuk <yosuzuk@users.noreply.github.com>
2023-03-17 23:52:08 +0100

    update indefatigable

--- src/data/ships/battleCruiser/indefatigable.ts
+++ src/data/ships/battleCruiser/indefatigable.ts
@@ -1,14 +1,75 @@
+import { flagshipEffect } from '../../../enhancements/enhancements';
 import { Manufacturer } from '../../../types/Manufacturer';
-import { IShipDefinition } from '../../../types/ShipDefinition';
+import { IDefaultShipStats, IShipDefinition, ISystemModule } from '../../../types/ShipDefinition';
 import { ShipRow } from '../../../types/ShipRow';
 import { ShipSource } from '../../../types/ShipSource';
 import { ShipType } from '../../../types/ShipType';
+import { modules } from '../../modules';
 import { ShipId } from '../../shipIds';
+
+const b1: ISystemModule = {
+    id: 'B1',
+    name: '護送艦ドック',
+    translatedName: {
+        en: 'Corvette Dock',
+    },
+    category: 'B',
+    categoryNumber: 1,
+    // carryCorvette: number; // TODO find out
+    // skillSlots?: number; // TODO find out
+};
+
+const b2: ISystemModule = {
+    id: 'B2',
+    name: '通常砲システム',
+    translatedName: {
+        en: 'Generic Battery System',
+    },
+    category: 'B',
+    categoryNumber: 2,
+    // carryCorvette: number; // TODO find out
+    // skillSlots?: number; // TODO find out
+    dpmShip: 3000,
+    dpmAntiAir: 360,
+    dpmSiege: 90,
+};
+
+const staticModules: ISystemModule[] = [
+    modules.commandSystem({
+        flagshipEffects: [
+            flagshipEffect.focusFire().withDefaultFlag(),
+        ],
+        // TODO skill
+        skillSlots: 1,
+    }),
+    modules.armorSystem({
+        // TODO skill
+        skillSlots: 6,
+    }),
+    modules.propulsionSystem({
+        // TODO skill
+        skillSlots: 3,
+    }),
+];
+
+const defaultStats: IDefaultShipStats = {
+    hp: 121350,
+    armor: 90,
+    shield: 15,
+    speed: 250,
+    warpSpeed: 1250,
+    dpmShip: 19996,
+    dpmAntiAir: 1701,
+    dpmSiege: 1889,
+};
 
 export const indefatigable: IShipDefinition[] = [
     {
         id: ShipId.INDEFATIGABLE,
         name: 'ルドゥタブル級',
+        translatedName: {
+            en: 'Indefatigable',
+        },
         type: ShipType.BATTLE_CRUISER,
         cost: 30,
         weight: 0,
@@ -17,6 +78,11 @@ export const indefatigable: IShipDefinition[] = [
         source: ShipSource.CITY_TRADE,
         manufacturer: Manufacturer.DAWN_ACCORD,
         relatedShipIds: [ShipId.INDEFATIGABLE_TE_S],
+        modules: [
+            b1, b2,
+            ...staticModules,
+        ],
+        defaultStats,
     },
     {
         id: ShipId.INDEFATIGABLE_TE_S,
@@ -29,5 +95,10 @@ export const indefatigable: IShipDefinition[] = [
         source: ShipSource.SALVAGE,
         manufacturer: Manufacturer.DAWN_ACCORD,
         relatedShipIds: [ShipId.INDEFATIGABLE],
+        modules: [
+            b1, b2,
+            ...staticModules,
+        ],
+        defaultStats,
     },
 ];
