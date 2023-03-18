9092407fc8f077b35451f216d031b1028494a7a3
yosuzuk <yosuzuk@users.noreply.github.com>
2023-03-18 01:49:06 +0100

    update thunderbolt

--- src/data/ships/battleCruiser/thunderboltStar.ts
+++ src/data/ships/battleCruiser/thunderboltStar.ts
@@ -2,7 +2,7 @@ import { enhancements, flagshipEffect } from '../../../enhancements/enhancements
 import { Manufacturer } from '../../../types/Manufacturer';
 import { ResearchStrategyType } from '../../../types/ResearchStrategyType';
 import { ResearchTacticType } from '../../../types/ResearchTacticType';
-import { IShipDefinition, ISystemModule } from '../../../types/ShipDefinition';
+import { IDefaultShipStats, IShipDefinition, ISystemModule } from '../../../types/ShipDefinition';
 import { ShipRow } from '../../../types/ShipRow';
 import { ShipSource } from '../../../types/ShipSource';
 import { ShipType } from '../../../types/ShipType';
@@ -12,6 +12,9 @@ import { ShipId } from '../../shipIds';
 const m1: ISystemModule = {
     id: 'M1',
     name: '艦首武器システム「スターオブサンダーボルト」',
+    translatedName: {
+        en: '"Thunderbold" Bow-Mounted Weapon System',
+    },
     description: '対小型武装',
     category: 'M',
     categoryNumber: 1,
@@ -28,11 +31,17 @@ const m1: ISystemModule = {
             '・直接、実弾、対艦：27353、攻城：5744',
         ],
     }],
+    dpmShip: 23785,
+    dpmAntiAir: 0,
+    dpmSiege: 4995,
 };
 
 const m2: ISystemModule = {
     id: 'M2',
     name: '艦首投射武器システム「スターオブサンダーボルト」',
+    translatedName: {
+        en: '"Thunderbold" Bow-Mounted Weapon System',
+    },
     description: '対大型武装',
     category: 'M',
     categoryNumber: 2,
@@ -49,11 +58,17 @@ const m2: ISystemModule = {
             'クリティカル',
         ],
     }],
+    dpmShip: 23625,
+    dpmAntiAir: 0,
+    dpmSiege: 4961,
 };
 
 const m3: ISystemModule = {
     id: 'M3',
     name: '「サンダーファイヤースター」弓高エネルギー兵器システム',
+    translatedName: {
+        en: '"Thunderbold" Bow High-Energy Weapon System',
+    },
     description: '対大型武装',
     category: 'M',
     categoryNumber: 3,
@@ -69,11 +84,17 @@ const m3: ISystemModule = {
             '・投射、エネルギー、対艦：39744、攻城：3312',
         ],
     }],
+    dpmShip: 28800,
+    dpmAntiAir: 0,
+    dpmSiege: 2880,
 };
 
 const a1: ISystemModule = {
     id: 'A1',
     name: '高速対艦武器システム',
+    translatedName: {
+        en: 'Rapid-Fire Anti-Ship Weapon System',
+    },
     category: 'A',
     categoryNumber: 1,
     defaultModule: true,
@@ -89,11 +110,17 @@ const a1: ISystemModule = {
             '反撃対空',
         ],
     }],
+    dpmShip: 12240,
+    dpmAntiAir: 7344,
+    dpmSiege: 1224,
 };
 
 const a2: ISystemModule = {
     id: 'A2',
     name: '中型対艦武器システム',
+    translatedName: {
+        en: 'Medium Anti-Ship Weapon System',
+    },
     description: '対小型武装',
     category: 'A',
     categoryNumber: 2,
@@ -108,11 +135,17 @@ const a2: ISystemModule = {
             '・直射、実弾、対艦：17884、攻城：1490',
         ],
     }],
+    dpmShip: 12960,
+    dpmAntiAir: 0,
+    dpmSiege: 1296,
 };
 
 const b1: ISystemModule = {
     id: 'B1',
     name: 'アクティブ対空システム',
+    translatedName: {
+        en: 'Active Anti-Aircraft System',
+    },
     category: 'B',
     categoryNumber: 1,
     defaultModule: true,
@@ -128,11 +161,17 @@ const b1: ISystemModule = {
             '対空支援',
         ],
     }],
+    dpmShip: 4500,
+    dpmAntiAir: 2700,
+    dpmSiege: 0,
 };
 
 const b2: ISystemModule = {
     id: 'B2',
     name: '範囲要撃システム',
+    translatedName: {
+        en: 'Range Interception System',
+    },
     description: '対空武装、ミサイル迎撃',
     category: 'B',
     categoryNumber: 2,
@@ -147,11 +186,17 @@ const b2: ISystemModule = {
             '迎撃効果',
         ],
     }],
+    dpmShip: 0,
+    dpmAntiAir: 1999,
+    dpmSiege: 0,
 };
 
 const c1: ISystemModule = {
     id: 'C1',
     name: '艦載軍需生産統合システム',
+    translatedName: {
+        en: 'Aircraft Military Industry Integration System',
+    },
     category: 'C',
     categoryNumber: 1,
     defaultModule: true,
@@ -173,6 +218,9 @@ const c1: ISystemModule = {
 const d1: ISystemModule = {
     id: 'D1',
     name: 'Weapon Coordination Center', // TODO JA
+    translatedName: {
+        en: 'Weapon Coordination Center',
+    },
     category: 'D',
     categoryNumber: 1,
     // skills: [
@@ -188,6 +236,9 @@ const d1: ISystemModule = {
 const d2: ISystemModule = {
     id: 'D2',
     name: 'Fire-Control Calibration System', // TODO JA
+    translatedName: {
+        en: 'Fire-Control Calibration System',
+    },
     category: 'D',
     categoryNumber: 2,
     // skillSlots: 4,
@@ -203,6 +254,9 @@ const d2: ISystemModule = {
 const e1: ISystemModule = {
     id: 'E1',
     name: '精密投射武器システム',
+    translatedName: {
+        en: 'Accurate Projectile Weapon System',
+    },
     description: '対小型武装',
     category: 'E',
     categoryNumber: 1,
@@ -218,11 +272,17 @@ const e1: ISystemModule = {
             '反撃対空',
         ],
     }],
+    dpmShip: 8437,
+    dpmAntiAir: 6749,
+    dpmSiege: 843,
 };
 
 const e2: ISystemModule = {
     id: 'E2',
     name: '大型発射体兵器システム',
+    translatedName: {
+        en: 'Large Projectile Weapon System',
+    },
     description: '対大型武装',
     category: 'E',
     categoryNumber: 2,
@@ -238,11 +298,17 @@ const e2: ISystemModule = {
             'クリティカル',
         ],
     }],
+    dpmShip: 8640,
+    dpmAntiAir: 0,
+    dpmSiege: 1814,
 };
 
 const f1: ISystemModule = {
     id: 'F1',
     name: '多目的武器システム',
+    translatedName: {
+        en: 'Multi-Target Weapon System',
+    },
     description: '対小型武装',
     category: 'F',
     categoryNumber: 1,
@@ -256,11 +322,17 @@ const f1: ISystemModule = {
             '反撃対空',
         ],
     }],
+    dpmShip: 6600,
+    dpmAntiAir: 5280,
+    dpmSiege: 660,
 };
 
 const f2: ISystemModule = {
     id: 'F2',
     name: '多目的対空システム',
+    translatedName: {
+        en: 'Multi-Target Anti-Air System',
+    },
     description: '対空武装',
     category: 'F',
     categoryNumber: 2,
@@ -274,6 +346,9 @@ const f2: ISystemModule = {
             '反撃対空',
         ],
     }],
+    dpmShip: 0,
+    dpmAntiAir: 3675,
+    dpmSiege: 0,
 };
 
 const staticModules: ISystemModule[] = [
@@ -295,6 +370,17 @@ const staticModules: ISystemModule[] = [
     modules.energySystem(),
 ];
 
+const defaultStats: IDefaultShipStats = {
+    hp: 130460,
+    armor: 120,
+    shield: 15,
+    speed: 400,
+    warpSpeed: 2000,
+    dpmShip: 46604,
+    dpmAntiAir: 11550,
+    dpmSiege: 7151,
+};
+
 export const thunderboldStar: IShipDefinition[] = [
     {
         id: ShipId.THUNDERBOLT_STAR,
@@ -314,6 +400,7 @@ export const thunderboldStar: IShipDefinition[] = [
             ShipId.THUNDERBOLT_STAR_TE_PREVIEW3,
         ],
         modules: [m1, m2, m3, a1, a2, b1, b2, c1, d1, d2, e1, e2, f1, f2, ...staticModules],
+        defaultStats,
     },
     {
         id: ShipId.THUNDERBOLT_STAR_TE_PREVIEW1,
@@ -332,6 +419,7 @@ export const thunderboldStar: IShipDefinition[] = [
             modules.toStatic(c1),
             ...staticModules,
         ],
+        defaultStats,
         relatedShipIds: [
             ShipId.THUNDERBOLT_STAR,
             ShipId.THUNDERBOLT_STAR_TE_PREVIEW2,
@@ -361,6 +449,7 @@ export const thunderboldStar: IShipDefinition[] = [
             modules.toStatic(e1),
             ...staticModules,
         ],
+        defaultStats,
     },
     {
         id: ShipId.THUNDERBOLT_STAR_TE_PREVIEW3,
@@ -385,5 +474,6 @@ export const thunderboldStar: IShipDefinition[] = [
             modules.toStatic(e2),
             ...staticModules,
         ],
+        defaultStats,
     },
 ];
