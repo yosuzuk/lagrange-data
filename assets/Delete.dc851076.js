a7af52810bab0f1fe3e93896c89639567f2c94b7
yosuzuk <yosuzuk@users.noreply.github.com>
2023-04-07 02:23:53 +0200

    Updates

---
+++ assets/Delete.dc851076.js
@@ -0,0 +1,1 @@
+import{P as a,Q as r,R as t}from"./index.e831ba06.js";var e={},u=r.exports;Object.defineProperty(e,"__esModule",{value:!0});var l=e.default=void 0,v=u(a),o=t,d=(0,v.default)((0,o.jsx)("path",{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"}),"Delete");l=e.default=d;export{l as d};
