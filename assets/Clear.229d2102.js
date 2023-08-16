c1358a911e4d8c254263d50fb524d26b0a2252c9
BuildTools <unconfigured@null.spigotmc.org>
2023-08-16 19:25:27 +0900

    Updates

--- assets/Clear.229d2102.js
+++
@@ -1,1 +0,0 @@
-import{Q as a,R as r,U as t}from"./index.bfc80d57.js";var e={},u=r.exports;Object.defineProperty(e,"__esModule",{value:!0});var l=e.default=void 0,o=u(a),d=t,v=(0,o.default)((0,d.jsx)("path",{d:"M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}),"Clear");l=e.default=v;export{l as d};
