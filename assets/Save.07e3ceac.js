4178ef67169bdeb71cd7ca7b4c263c9fa24ee294
BuildTools <unconfigured@null.spigotmc.org>
2023-11-20 00:52:55 +0900

    Updates

---
+++ assets/Save.07e3ceac.js
@@ -0,0 +1,1 @@
+import{Q as a,R as r,U as t}from"./index.232c7363.js";var e={},u=r.exports;Object.defineProperty(e,"__esModule",{value:!0});var v=e.default=void 0,o=u(a),d=t,l=(0,o.default)((0,d.jsx)("path",{d:"M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"}),"Save");v=e.default=l;export{v as d};
